94b8c279935c5e186c455f861d3a93063c1807d4
honeypathkar <honeypatkar70@gmail.com>
2024-10-19 12:59:10 +0530

    Creating store for stroring item

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,7 +1,10 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {useStore} from '../store/store';
 
 const HomeScreen = () => {
+  const CoffeeList = useStore((state: any) => state.CoffeeList);
+  console.log(CoffeeList.length);
   return (
     <View>
       <Text>HomeScreen</Text>
