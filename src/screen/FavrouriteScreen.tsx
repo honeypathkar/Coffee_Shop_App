eb7559558f569059b2b3716f0779a79bdcf3e983
honeypathkar <honeypatkar70@gmail.com>
2024-10-28 14:42:41 +0530

    Favourite screen completed

--- src/screen/FavrouriteScreen.tsx
+++ src/screen/FavrouriteScreen.tsx
@@ -1,14 +1,111 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  ToastAndroid,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
+import {useStore} from '../store/store';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+import {COLORS, SPACING} from '../theme/theme';
+import HeaderBar from '../components/HeaderBar';
+import EmptyListAnimation from '../components/EmptyListAnimation';
+import FavoritesItemCard from '../components/FavouriteItemCard';
 
-const Favrourite = () => {
+const Favrourite = ({navigation}: any) => {
+  const FavouritesList = useStore((state: any) => state.FavouritesList);
+  const tabBarHeight = useBottomTabBarHeight();
+  const addToFavoriteList = useStore((state: any) => state.addToFavoriteList);
+  const deleteFromFavoriteList = useStore(
+    (state: any) => state.deleteFromFavoriteList,
+  );
+  const ToggleFavourite = (favourite: boolean, type: string, id: string) => {
+    favourite ? deleteFromFavoriteList(type, id) : addToFavoriteList(type, id);
+    favourite
+      ? ToastAndroid.showWithGravity(
+          'Removed from Favourite',
+          ToastAndroid.SHORT,
+          ToastAndroid.CENTER,
+        )
+      : ToastAndroid.showWithGravity(
+          'Added to Favourite',
+          ToastAndroid.SHORT,
+          ToastAndroid.CENTER,
+        );
+  };
   return (
-    <View>
-      <Text>Favrourite</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}>
+        <View
+          style={[styles.ScrollViewInnerView, {marginBottom: tabBarHeight}]}>
+          <View style={styles.ItemContainer}>
+            <HeaderBar title="Favourites" />
+
+            {FavouritesList.length == 0 ? (
+              <EmptyListAnimation title={'No Favourites'} />
+            ) : (
+              <View style={styles.ListItemContainer}>
+                {FavouritesList.map((data: any) => (
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.push('Details', {
+                        index: data.index,
+                        id: data.id,
+                        type: data.type,
+                      });
+                    }}
+                    key={data.id}>
+                    <FavoritesItemCard
+                      id={data.id}
+                      imagelink_portrait={data.imagelink_portrait}
+                      name={data.name}
+                      special_ingredient={data.special_ingredient}
+                      type={data.type}
+                      ingredients={data.ingredients}
+                      average_rating={data.average_rating}
+                      ratings_count={data.ratings_count}
+                      roasted={data.roasted}
+                      description={data.description}
+                      favourite={data.favourite}
+                      ToggleFavouriteItem={ToggleFavourite}
+                    />
+                  </TouchableOpacity>
+                ))}
+              </View>
+            )}
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
 export default Favrourite;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+  ScrollViewInnerView: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  ItemContainer: {
+    flex: 1,
+  },
+  ListItemContainer: {
+    paddingHorizontal: SPACING.space_20,
+    gap: SPACING.space_20,
+  },
+});
