d5e7c7e1f8776aae1f6aaf8fad3fb344a6d6c73e
honeypathkar <honeypatkar70@gmail.com>
2024-11-06 01:44:47 +0530

    Creating OrderHistory page

--- src/screen/OrderHistoryScreen.tsx
+++ src/screen/OrderHistoryScreen.tsx
@@ -1,14 +1,133 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+import {useStore} from '../store/store';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import EmptyListAnimation from '../components/EmptyListAnimation';
+import HeaderBar from '../components/HeaderBar';
+import PopUpAnimation from '../components/PopUpAnimation';
+import OrderHistoryCard from '../components/OrderHistoryCard';
 
-const OrderHistoryScreen = () => {
+const OrderHistoryScreen = ({navigation}: any) => {
+  const OrderHistoryList = useStore((state: any) => state.OrderHistoryList);
+  const tabBarHeight = useBottomTabBarHeight();
+
+  const navigationHandler = ({index, id, type}: any) => {
+    navigation.push('Details', {
+      index,
+      id,
+      type,
+    });
+  };
+
+  const buttonPressHandler = () => {
+    setShowAnimation(true);
+    setTimeout(() => {
+      setShowAnimation(false);
+    }, 2000);
+  };
+
+  const [showAnimation, setShowAnimation] = useState(false);
   return (
-    <View>
-      <Text>OrderHistoryScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      {showAnimation ? (
+        <PopUpAnimation
+          style={styles.LottieAnimation}
+          source={require('../lottie/download.json')}
+        />
+      ) : (
+        <></>
+      )}
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={styles.ScrollViewFlex}>
+        <View
+          style={[styles.ScrollViewInnerView, {marginBottom: tabBarHeight}]}>
+          <View style={styles.ItemContainer}>
+            <HeaderBar title="Order History" />
+
+            {OrderHistoryList.length == 0 ? (
+              <EmptyListAnimation title={'No Order Yet'} />
+            ) : (
+              <View style={styles.ListItemContainer}>
+                {OrderHistoryList.map((data: any, index: any) => (
+                  <OrderHistoryCard
+                    key={index.toString()}
+                    navigationHandler={navigationHandler}
+                    CartListPrice={data.CartListPrice}
+                    CartList={data.CartList}
+                    OrderDate={data.OrderDate}
+                  />
+                ))}
+              </View>
+            )}
+          </View>
+          {OrderHistoryList.length > 0 ? (
+            <TouchableOpacity
+              style={styles.DownloadButton}
+              onPress={() => {
+                buttonPressHandler();
+              }}>
+              <Text style={styles.ButtonText}>Download</Text>
+            </TouchableOpacity>
+          ) : (
+            <></>
+          )}
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
 export default OrderHistoryScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+  ScrollViewInnerView: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  ItemContainer: {
+    flex: 1,
+  },
+  ListItemContainer: {
+    paddingHorizontal: SPACING.space_20,
+    gap: SPACING.space_20,
+  },
+  LottieAnimation: {
+    height: 250,
+  },
+  DownloadButton: {
+    margin: SPACING.space_20,
+    backgroundColor: COLORS.primaryOrangeHex,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: SPACING.space_36 * 2,
+    borderRadius: BORDERRADIUS.radius_20,
+  },
+  ButtonText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.primaryWhiteHex,
+  },
+});
